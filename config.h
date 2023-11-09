c1383cfbe3d5f1d06d70fa039f4a69f47aa58c2a
David Anguita <david@davidanguita.name>
2023-11-09 18:51:24 +0100

    Remove cpu_perc and cat platform_profile segments

--- config.h
+++ config.h
@@ -67,11 +67,9 @@ static const struct arg args[] = {
 	/* function format          argument */
 	{ wifi_perc         , " %s%%"   , "wlp0s20f3" }                           ,
 	{ wifi_essid        , " %s"     , "wlp0s20f3" }                           ,
-	{ cpu_perc          , " | %s%%" , NULL }                                  ,
 	{ ram_used          , " | %s"   , NULL }                                  ,
 	{ battery_perc      , " | %s%%" , "BAT0" }                                ,
 	{ battery_state     , " %s"     , "BAT0" }                                ,
 	{ battery_remaining , " %s"     , "BAT0" }                                ,
-	{ cat               , " %.3s"   , "/sys/firmware/acpi/platform_profile" } ,
 	{ datetime          , " | %s"   , "%a %b %d %H:%M:%S " }
 };
